5b69eb352fec932ebbe11b40242b2fdbc7111a02
Toshiro Inami <111782241+ToshiroInami@users.noreply.github.com>
2024-07-17 17:42:37 +0000

    actualizacion

--- src/main/java/pe/edu/vallegrande/vg_ms_communion/rest/CommunionController.java
+++ src/main/java/pe/edu/vallegrande/vg_ms_communion/rest/CommunionController.java
@@ -13,6 +13,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/communions")
 public class CommunionController {
 
